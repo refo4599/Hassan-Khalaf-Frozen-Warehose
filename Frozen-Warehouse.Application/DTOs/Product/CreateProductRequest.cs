4d06cebcceb4aa3fd11c3e73e6cdc59dbdc9260e
alaasaber <alaasabermostafa9@gmail.com>
2026-01-25 19:38:07 +0200

    product controller

---
+++ Frozen-Warehouse.Application/DTOs/Product/CreateProductRequest.cs
@@ -0,0 +1,7 @@
+namespace Frozen_Warehouse.Application.DTOs.Product
+{
+    public class CreateProductRequest
+    {
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Frozen-Warehouse.Application/DTOs/Product/ProductDto.cs
@@ -0,0 +1,8 @@
+namespace Frozen_Warehouse.Application.DTOs.Product
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
